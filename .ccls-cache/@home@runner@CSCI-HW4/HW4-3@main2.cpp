868d1418adb5e0d5eaaf6859bf48446c1cf0f6dc
jrchaney <111770024+jrchaney@users.noreply.github.com>
2022-09-14 00:09:39 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@CSCI-HW4/HW4-3@main2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+
+  int cases, N, T;
+  
+  
+  cin >> cases;
+  while (cases--){
+    cin >> T;
+    N = T * 7;
+    cout << "N = " << N ; 
+    cout << "\n";
+    
+    
+  }
+  
+
+  
+  
+  
+}
---
+++ HW4-2/main1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+
+  int a, b;
+  cin >> a >> b;
+
+
+  if(a > b){
+    cout << "The first number is greater" << endl;
+    }
+  else if(a < b){
+    cout << "The first number is smaller" << endl;
+  }
+
+  return 0;
+  
+}
